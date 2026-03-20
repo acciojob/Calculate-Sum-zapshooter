fd42b118268c8dd6e00960952595e08cc89df6fa
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-03-20 22:57:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById('sum_btn').addEventListener('click', function () {
+	var n1 = parseFloat(document.getElementById('n1').value);
+	var n2 = parseFloat(document.getElementById('n2').value);
+	var sumEl = document.getElementById('sum');
+
+	if (isNaN(n1) || isNaN(n2)) {
+		sumEl.innerHTML = '';
+		return;
+	}
+
+	var result = n1 + n2;
+
+	sumEl.innerHTML =
+		'<div class="result-label">Sum</div>' +
+		'<div class="result-value">' + result + '</div>';
+});
